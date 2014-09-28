1fec5477c05c48e0e6cc20f915afd232b7e0c242
elij <elij@wundrd.net>
2014-09-28 14:05:04 -0700

    fix example time

--- example_test.go
+++ example_test.go
@@ -11,5 +11,5 @@ func ExampleFormat() {
 	t := time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)
 	fmt.Println(gostrftime.Format("%Y-%m-%d", t))
 	// Output:
-	// 2009-01-02
+	// 2009-11-10
 }
