7c9042f24aa205e3bc198fd755425fc0929972f9
elij <elij@wundrd.net>
2016-05-12 09:28:55 -0700

    revert

--- example_test.go
+++ example_test.go
@@ -2,16 +2,18 @@
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file.
 
-package gostrftime
+package gostrftime_test
 
 import (
 	"fmt"
 	"time"
+
+	"github.com/cactus/gostrftime"
 )
 
 func ExampleFormat() {
 	t := time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)
-	fmt.Println(Strftime("%Y-%m-%d", t))
+	fmt.Println(gostrftime.Format("%Y-%m-%d", t))
 	// Output:
 	// 2009-11-10
 }
